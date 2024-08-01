081eec47d5ab170f033ac081d8c20efc8180e84a
claudiahW <murungiclaud@gmail.com>
2024-08-01 13:26:40 +0300

    align search bar and search icon

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import "./Weather.css";
+import search_icon from "../assets/images/search.png";
 
 const Weather = () => {
   return (
-    <div className="">
-    
+    <div className="weather">
+      <div className="searchbar">
+        <input type="text" placeholder="Search" />
+        <img src={search_icon} alt="" />
+      </div>
     </div>
   );
 };
