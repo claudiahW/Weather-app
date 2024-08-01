6b874ecbd56cf05a9d8bdac711ade311d8fd6ae4
claudiahW <murungiclaud@gmail.com>
2024-08-01 14:02:33 +0300

    add weather in celcius

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,6 +1,13 @@
 import React from "react";
 import "./Weather.css";
 import search_icon from "../assets/images/search.png";
+import clear_icon from "../assets/images/clear.png";
+import cloud_icon from "../assets/images/clouds.png";
+import drizzle_icon from "../assets/images/drizzle.png";
+import rain_icon from "../assets/images/rain.png";
+import snow_icon from "../assets/images/snow.png";
+import wind_icon from "../assets/images/wind.png";
+import humidity_icon from "../assets/images/humidity.png";
 
 const Weather = () => {
   return (
@@ -9,6 +16,10 @@ const Weather = () => {
         <input type="text" placeholder="Search" />
         <img src={search_icon} alt="" />
       </div>
+
+      <img src={clear_icon} alt="" />
+      <p>16°C</p>
+      <p>London</p>
     </div>
   );
 };
