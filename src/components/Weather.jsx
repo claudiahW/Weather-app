ff201faad7e2c01e22a1cf40095757718de66599
claudiahW <murungiclaud@gmail.com>
2024-08-02 18:42:44 +0300

    add a function to prompt a user to enter a city if the input is empty

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -31,6 +31,12 @@ const Weather = () => {
   };
 
   const search = async (city) => {
+
+if(city === ""){
+  alert("Enter City Name");
+  return;
+}
+
     try {
       const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${
         import.meta.env.VITE_APP_ID
